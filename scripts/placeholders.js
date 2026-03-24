713d51b3d193f4daaf8522c3b05bcf578c08a0ca
prachit20 <prachit.pandit@deptagency.com>
2026-03-24 15:46:43 +0530

    Chnages

--- scripts/placeholders.js
+++ scripts/placeholders.js
@@ -1,10 +1,10 @@
 export default async function fetchPlaceholders() {
   return {
-    carousel: "Carousel",
-    carouselSlideControls: "Carousel Slide Controls",
-    previousSlide: "Previous Slide",
-    nextSlide: "Next Slide",
-    showSlide: "Show Slide",
-    of: "of",
+    carousel: 'Carousel',
+    carouselSlideControls: 'Carousel Slide Controls',
+    previousSlide: 'Previous Slide',
+    nextSlide: 'Next Slide',
+    showSlide: 'Show Slide',
+    of: 'of',
   };
 }
